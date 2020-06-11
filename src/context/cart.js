3155584946f766f617cd99c39fe294c6059b5b0f
‘Juliano <julianoperins@gmail.com>
2020-06-11 19:12:05 -0400

    set up empty cart

--- src/context/cart.js
+++ src/context/cart.js
@@ -6,7 +6,7 @@ import localCart from "../utils/localCart";
 const CartContext = createContext();
 
 function CartProvider({ children }) {
-  const [cart, setCart] = useState(localCart);
+  const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
   const [cartItems, setCartItems] = useState(0);
   return (
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { CartContext } from "../context/cart";
+import EmptyCart from "../components/Cart/EmptyCart";
+import CartItem from "../components/Cart/CartItem";
+
+import { Link } from "react-router-dom";
+// import {useContext} from '../context/users'
 
 export default function Cart() {
+  let user = false;
   const { cart, total } = React.useContext(CartContext);
-  console.log(cart, total);
+
+  if (cart.length === 0) {
+    return <EmptyCart />;
+  }
 
   return <h1>hello from cart page</h1>;
 }
