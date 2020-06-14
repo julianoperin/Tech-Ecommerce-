f24f066228ac824313559b6811f06c7a4c94d77f
‘Juliano <julianoperins@gmail.com>
2020-06-14 04:03:01 -0400

    set up clouldinary and prepared the pictures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,13 @@
 import url from "./URL";
 
 //! Flatten
-export function flattenProducts() {}
+export function flattenProducts(data) {
+  return data.map((item) => {
+    //Cloudinary
+    let image = item.image.url;
+    return { ...item, image };
+  });
+}
 
 //! helper functions
 export function featuredProducts(data) {
