7c99d248dfc7e91bf2df98b3ccc955be47439e58
‘Juliano <julianoperins@gmail.com>
2020-06-15 14:52:59 -0400

    worked on Login component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Login() {
+// Strapi function
+
+// Handle user
+import { useHistory } from "react-router-dom";
+
+const Login = () => {
+  const history = useHistory();
+  //setup user context
+
+  //state values
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userName, setUsername] = useState("default");
+  const [isMember, setMember] = useState(true);
+
+  let isEmpty = false;
+
+  const toggleMember = () => {};
+
+  const handleSubmit = async (e) => {};
+
   return <h1>hello from login page</h1>;
-}
+};
+
+export default Login;
